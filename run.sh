0052df50e43c76fd9421aca3bd7b456b9a66394a
Mitrofanov Anton <mitrofanov-aa@speechpro.com>
2023-11-21 19:34:39 +0300

    run.sh

--- run.sh
+++ run.sh
@@ -21,13 +21,13 @@ help_message="$0 [options] #TODO"
 if [ $stage -le 0 ] ; then
     echo "$0: Stage 0: Split data"
 
-     #python maatool/cli/split_json.py \
-     #   data/valid.jsonl \
-     #   $(for i in $(seq $test_nj) ; do echo -n " data/split$test_nj/valid.$i.json" ; done)
+     python maatool/cli/split_json.py \
+        data/valid.jsonl \
+        $(for i in $(seq $test_nj) ; do echo -n " data/split$test_nj/valid.$i.json" ; done)
 
-     #python maatool/cli/split_json.py \
-     #   data/test.jsonl \
-     #   $(for i in $(seq $test_nj) ; do echo -n " data/split$test_nj/test.$i.json" ; done)
+     python maatool/cli/split_json.py \
+        data/test.jsonl \
+        $(for i in $(seq $test_nj) ; do echo -n " data/split$test_nj/test.$i.json" ; done)
 
     python maatool/cli/split_json.py \
         data/train.jsonl \
@@ -37,11 +37,11 @@ fi
 
 if [ $stage -le 1 ] ; then 
     echo "$0: Stage 1: extract feats"
-    #$cmd JOB=1:$test_nj data_feats/valid/log/feats.JOB.log \
-    #    python maatool/cli/extract_feats_from_json.py \
-    #        --uid_basename valid-JOB \
-    #        data/split$test_nj/valid.JOB.json \
-    #        ark,scp:data_feats/valid/feats.JOB.ark,data_feats/valid/feats.JOB.scp 
+    $cmd JOB=1:$test_nj data_feats/valid/log/feats.JOB.log \
+        python maatool/cli/extract_feats_from_json.py \
+            --uid_basename valid-JOB \
+            data/split$test_nj/valid.JOB.json \
+            ark,scp:data_feats/valid/feats.JOB.ark,data_feats/valid/feats.JOB.scp 
     [ ! -d data_feats/valid/ ] && mkdir -p data_feats/valid/
     python maatool/cli/extract_feats_from_json.py \
         data/valid.jsonl \
@@ -49,16 +49,15 @@ if [ $stage -le 1 ] ; then
 
 
 
-    #$cmd JOB=1:$test_nj data_feats/test/log/feats.JOB.log \
-    #    python maatool/cli/extract_feats_from_json.py \
-    #        --uid_basename test-JOB \
-    #        data/split$test_nj/test.JOB.json \
-    #        ark,scp:data_feats/test/feats.JOB.ark,data_feats/test/feats.JOB.scp 
+    $cmd JOB=1:$test_nj data_feats/test/log/feats.JOB.log \
+        python maatool/cli/extract_feats_from_json.py \
+            --uid_basename test-JOB \
+            data/split$test_nj/test.JOB.json \
+            ark,scp:data_feats/test/feats.JOB.ark,data_feats/test/feats.JOB.scp 
     [ ! -d data_feats/test/ ] && mkdir -p data_feats/test/
     python maatool/cli/extract_feats_from_json.py \
         data/test.jsonl \
         ark,scp:data_feats/test/feats.ark,data_feats/test/feats.scp 
-
 
 
     $cmd JOB=1:$train_nj data_feats/train/log/feats.JOB.log \
@@ -81,34 +80,33 @@ if [ $stage -le 2 ] ; then
     cat data_feats/train/text.* | sort -n > data_feats/train/text
 fi
 
+ if [ $stage -le 3 ] ; then 
+     echo "$0 Stage 3: prepare additional data"
+ 
+     for n in accepted suggestion_accepted ; do 
+     #for n in suggestion_accepted ; do 
+         python maatool/cli/split_json.py \
+             data/$n \
+             $(for i in $(seq $train_nj) ; do echo -n " data/split$train_nj/$n.$i.json" ; done)
+ 
+         python maatool/cli/split_json.py \
+             data/$n \
+             $(for i in $(seq $train_nj) ; do echo -n " data/split$train_nj/$n.$i.json" ; done)
 
-
-if [ $stage -le 3 ] ; then 
-    echo "$0 Stage 3: prepare additional data"
-
-    for n in accepted suggestion_accepted ; do 
-    #for n in suggestion_accepted ; do 
-        python maatool/cli/split_json.py \
-            data/$n \
-            $(for i in $(seq $train_nj) ; do echo -n " data/split$train_nj/$n.$i.json" ; done)
-
-        python maatool/cli/split_json.py \
-            data/$n \
-            $(for i in $(seq $train_nj) ; do echo -n " data/split$train_nj/$n.$i.json" ; done)
-
-        $cmd JOB=1:$train_nj data_feats/$n/log/feats.JOB.log \
-            python maatool/cli/extract_feats_from_json.py \
-            data/split$train_nj/$n.JOB.json \
-            ark,scp:data_feats/$n/feats.JOB.ark,data_feats/$n/feats.JOB.scp || touch data_feats/$n/.error
-
-        $cmd JOB=1:$train_nj data_feats/$n/log/text.JOB.log \
-            python maatool/cli/text_from_json.py \
-            data/split$train_nj/$n.JOB.json \
-            data_feats/$n/text.JOB
-
-        cat data_feats/$n/text.* | sort -n > data_feats/$n/text
-    done
-fi
+         ## I use additional data only for sentencepiece model
+         #$cmd JOB=1:$train_nj data_feats/$n/log/feats.JOB.log \
+         #    python maatool/cli/extract_feats_from_json.py \
+         #    data/split$train_nj/$n.JOB.json \
+         #    ark,scp:data_feats/$n/feats.JOB.ark,data_feats/$n/feats.JOB.scp || touch data_feats/$n/.error
+ 
+         $cmd JOB=1:$train_nj data_feats/$n/log/text.JOB.log \
+             python maatool/cli/text_from_json.py \
+             data/split$train_nj/$n.JOB.json \
+             data_feats/$n/text.JOB
+ 
+         cat data_feats/$n/text.* | sort -n > data_feats/$n/text
+     done
+ fi
 
 
 if [ $stage -le 4 ] ; then 
@@ -159,4 +157,11 @@ if [ $stage -le 6 ] ; then
     done
 fi
 
+
+if [ $stage -le 7 ] ; then 
+    echo "$0: Stage 7: Run train"
+    python ./train_conformer_v1.py
+fi
+
+echo "$0: What's all. Load checkpoint in after_deadline.ipynb and make submit"
 exit
